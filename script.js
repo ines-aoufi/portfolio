cfe30d1eeb62a0027f652203acbf64b78b418ac9
Inès AOUFI <inesaoufii@gmail.com>
2025-04-04 08:18:54 +0200

    add stickers and photo

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('imageContainer').addEventListener('click', function() {
+    const overlayImage = document.querySelector('.overlay-img');
+    overlayImage.classList.toggle('show'); // Toggle the 'show' class on click
+});
